09a493fa5fd6e2fcbabc674ed97d77b28719ef9e
elasa7 <elasa7@gmail.com>
2023-09-01 10:38:36 +0200

    working 0.2v

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+// Select the container for skeleton elements and the JSON API URL.
 const wraper = document.querySelector(".skeleton-wraper");
 const jsonURL = "https://rickandmortyapi.com/api/character/";
-const startSkeletonCount = 20;
+
+// Set the initial number of skeleton items to load, and create an array to store them.
+let startSkeletonCount = 5;
 const createdSkeletonArray = [];
 
+// Track the current item count and store fetched image URLs.
+let currentItemCount = 0;
+let imageUrls = [];
+
+// Select the "Load More" button.
+const loadMoreButton = document.querySelector(".load-more-btn");
+
+// Default animation settings for skeleton items.
 const animationDefault = {
   animationName: "fade",
   delayMultiple: 0.5,
@@ -12,10 +23,12 @@ const animationDefault = {
   type: "forwards",
 };
 
+// Function to merge new animation settings with defaults.
 function newAnimeSetting(newSetting) {
   return Object.assign({}, animationDefault, newSetting);
 }
 
+// Function to create a skeleton item with a wrapper, skeleton, and image.
 function makeSkeletonItem() {
   const skeletonWrapper = document.createElement("div");
   const skeleton = document.createElement("div");
@@ -32,29 +45,34 @@ function makeSkeletonItem() {
   return { skeleton, skeletonImage };
 }
 
+// Function to set animation properties for a given element.
 function setAnimation(element, animSett, delayMultiple = 0) {
   element.style.animation = `${animSett.animationName} ${animSett.delay}s ${animSett.easing} ${animSett.type}`;
   element.style.animationDelay = `${delayMultiple * animSett.stagDealy}s`;
 }
 
+// Function to apply an animation to fade out all skeleton elements.
 function fadeAllSkeletons() {
   createdSkeletonArray.forEach((skeleton) => {
     setAnimation(skeleton.skeleton, newAnimeSetting({ animationName: "fadeAll", delay: 2, type: "infinite" }));
   });
 }
 
+// Function to set image sources and handle their load events.
 async function setImages(imageUrls) {
   const skeletonImages = document.querySelectorAll(".skeleton-item-image");
 
   skeletonImages.forEach((element, index) => {
     element.src = imageUrls[index].image;
     element.addEventListener("load", () => {
+      element.classList.add("fadeImage");
       element.style.display = "inline-block";
       createdSkeletonArray[index].skeleton.style.display = "none";
     });
   });
 }
 
+// Function to fetch JSON data from the specified URL.
 async function fetchData(url) {
   try {
     const response = await fetch(url);
@@ -69,6 +87,7 @@ async function fetchData(url) {
   }
 }
 
+// Function to initialize the page by creating initial skeleton items and loading images.
 async function initialize() {
   for (let i = 0; i < startSkeletonCount; i++) {
     const { skeleton, skeletonImage } = makeSkeletonItem();
@@ -81,8 +100,27 @@ async function initialize() {
     }
   }
 
-  const imageUrls = await fetchData(jsonURL);
-  setImages(imageUrls);
+  if (imageUrls.length === 0) {
+    imageUrls = await fetchData(jsonURL);
+    setImages(imageUrls);
+  } else {
+    setImages(imageUrls);
+  }
 }
 
+// Event listener for the "Load More" button. disable button after last part of image was loaded
+loadMoreButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  currentItemCount += startSkeletonCount;
+  if (currentItemCount < imageUrls.length) {
+    initialize();
+  }
+  if (currentItemCount >= imageUrls.length - startSkeletonCount) {
+    loadMoreButton.style.display = "none";
+  } else {
+    return;
+  }
+});
+
+// Initialize the page.
 initialize();
